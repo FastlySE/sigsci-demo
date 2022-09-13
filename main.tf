68f7f3905b4136b265352dee5f9320455c3c0cbc
FastlySE <113071464+FastlySE@users.noreply.github.com>
2022-09-13 15:54:16 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -367,3 +367,52 @@ corp_scope       = "global"
 }
 #### end robots.txt
 
+resource "sigsci_corp_signal_tag" "ofac" {
+  short_name  = "ofac"
+  description = "Countries on OFAC list"
+}
+
+resource "sigsci_corp_list" "ofac" {
+    name = "OFAC Countries"
+    type = "country"
+    entries = [
+        "IR",
+        "SY",
+        "SD",
+        "KP",
+        "BY",
+        "CI",
+        "CU",
+        "CD",
+        "IQ",
+        "LR",
+        "MM",
+        "ZW",
+    ]
+}
+
+resource "sigsci_corp_rule" "ofac" {
+  site_short_names = []
+  type = "request"
+  corp_scope = "global"
+  enabled = true
+  group_operator = "all"
+  reason = "OFAC Country Blocking Rule"
+  expiration = ""
+
+  conditions {
+    type     = "single"
+    field    = "country"
+    operator = "inList"
+    value = "corp.ofac-countries"
+  }
+
+  actions {
+    type = "block"
+  }
+
+  actions {
+    type = "addSignal"
+    signal = "corp.ofac" 
+  }
+}
