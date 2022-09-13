a796ff90dc3eceeb4957d273067f4d669e6fd739
FastlySE <113071464+FastlySE@users.noreply.github.com>
2022-09-13 15:55:49 +1000

    Domain Rule

--- main.tf
+++ main.tf
@@ -416,3 +416,39 @@ resource "sigsci_corp_rule" "ofac" {
     signal = "corp.ofac" 
   }
 }
+
+resource "sigsci_corp_signal_tag" "domain-signal" {
+  short_name  = "domain-request"
+  description = "Tagging requests to domain"
+}
+
+resource "sigsci_corp_list" "domain-list" {
+    name = "Domain List"
+    type = "wildcard"
+    entries = [ // Change values in this list to reflect your domain
+        "example.com",
+        "*.example.com", 
+    ]
+}
+
+resource "sigsci_corp_rule" "domain-rule" {
+  site_short_names = []
+  type = "request"
+  corp_scope = "global"
+  enabled = true
+  group_operator = "all"
+  reason = "Identify requests with valid domain in host header"
+  expiration = ""
+
+  conditions {
+    type     = "single"
+    field    = "domain"
+    operator = "inList"
+    value = "corp.domain-list"
+  }
+
+  actions {
+    type   = "addSignal"
+    signal = "corp.domain-request" 
+  }
+}
