5b8bcd7bdc1755ff4533d099eafb5e1a89772fdf
FastlySE <113071464+FastlySE@users.noreply.github.com>
2022-12-15 14:24:51 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,6 +43,52 @@ resource "sigsci_corp_signal_tag" "attack-sus-src" {
 }
 
 ##Cut
+resource "sigsci_corp_signal_tag" "bad-ua" {
+  short_name  = "bad-ua"
+  description = "Known bad User Agents block"
+}
+
+resource "sigsci_corp_list" "bad-ua" {
+    name = "Bad UA"
+    type = "wildcard"
+    entries = [
+        "*[Cc][Uu][Rr][Ll]*",
+        "*[Pp][Yy][Tt][Hh][Oo][Nn]*",
+        "*[Ww][Pp][Ss][Cc][Aa][Nn]*",
+        "*[Nn][Mm][Aa][Pp]*",
+        "*[Mm][Aa][Ss][Ss][Cc][Aa][Nn]*",
+    ]
+}
+
+resource "sigsci_corp_rule" "bad-ua" {
+    site_short_names = []
+    type = "request"
+    corp_scope = "global"
+    enabled = true
+    group_operator = "all"
+    reason = "Bad User Agents Blocking Rule"
+    expiration = ""
+
+    conditions {
+      type     = "single"
+      field    = "useragent"
+      operator = "inList"
+      value = "corp.bad-ua"
+    }
+
+    actions {
+    type = "block"
+    }
+
+    actions {
+    type = "addSignal"
+    signal = "corp.bad-ua" 
+  }
+  depends_on = [
+  sigsci_corp_signal_tag.bad-ua
+  ]
+}
+
 
 
 #### end cut
