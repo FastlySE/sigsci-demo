423b940c5f2f795f5aaf8b77d94a0bdeb1c1f160
FastlySE <113071464+FastlySE@users.noreply.github.com>
2022-12-22 17:16:34 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -295,8 +295,9 @@ resource "sigsci_corp_rule" "any-attack-signal-rule" {
 ### start OWASP-Attack rule site specific
 
 resource "sigsci_site_signal_tag" "owasp-attack-signal" {
-  short_name      = "owasp-attack-signal"
-  description     = "Flag on attack signals"
+  site_short_name = sigsci_site.personal-apps.owasp-attack-signal
+  name            = "OWASP signal tag"
+  description     = "OWASP Signal Tag"
 }
 
 resource "sigsci_site_rule" "OWASP-attack-signal-rule" {
